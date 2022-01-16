f545a216267090fbdc23c9898e1d14f387d17a5c
Corey DeLoach <cdeloach16@gmail.com>
2022-01-16 09:56:35 -0600

    fixed single quotes error

--- pages/people.jsx
+++ pages/people.jsx
@@ -96,14 +96,14 @@ const People = () => {
             I grew up in Cleveland, MS (home of the Fighting Okra, yes look it
             up). I attended Mississippi State University for my undergraduate
             degree in Biological Sciences and Mississippi College for my
-            master’s degree in Biomedical Sciences. I have worked for the
+            master&apos;s degree in Biomedical Sciences. I have worked for the
             University of Mississippi Medical Center (UMMC) as a researcher for
             7 years studying adolescent pediatric scoliosis in the pediatric
             population before returning to Mississippi State University for my
             doctorate degree in molecular biology and biochemistry. I worked for
             the USDA-ARS as a post-doctoral f ellow for 3 years as a geneticist
-            prior to joining Dr. Yarar-Fisher’s team where I am the laboratory
-            manager and a researcher. Dr. Yarar-Fisher’s program investigating
+            prior to joining Dr. Yarar-Fisher&apos;s team where I am the laboratory
+            manager and a researcher. Dr. Yarar-Fisher&apos;s program investigating
             spinal cord injury compliments some techniques that I have learned
             along my academic and professional career but, most importantly, has
             encompassed many new and exciting disciplines and techniques
@@ -140,7 +140,7 @@ const People = () => {
           </CenterDiv>
           <P>
             I am a 4th year post-doctoral scholar at Rehabilitation Medicine
-            Laboratory. I am from China, and I earned my master’s degree in
+            Laboratory. I am from China, and I earned my master&apos;s degree in
             Molecular Genetics from the University of Cincinnati and my PhD
             degree in Nutrition Sciences from Purdue University. During my PhD
             training, I was involved with clinical trials investigating several
@@ -148,7 +148,7 @@ const People = () => {
             greatly appreciated the applicability of human clinical nutrition
             research. In the meanwhile, I became interested in exploring the
             underlying mechanisms for the development of cardio-metabolic
-            diseases. I joined Dr. Yarar-Fisher’s lab in 2017 to further expand
+            diseases. I joined Dr. Yarar-Fisher&apos;s lab in 2017 to further expand
             my training in nutrition research and gain additional skillset in
             microbiome and metabolomics analyses. My current research projects
             include: 1) investigating the effects of a
@@ -180,13 +180,13 @@ const People = () => {
             />
           </CenterDiv>
           <P>
-            I received my Bachelor’s degree from KSU in Saudi Arabia, and my
+            I received my Bachelor&apos;s degree from KSU in Saudi Arabia, and my
             Master of Science degree in the neuromuscular field from the
             University of Pittsburgh, Pennsylvania in 2016. I have worked as a
             physical therapist with a focus on neurological rehabilitation and
             as a TA at Qassim University. Currently, I am a doctoral student in
             the Rehabilitation Science Program at UAB, and I joined Dr.
-            Yarar-Fisher’s lab in Fall 2019. My primary research interest is
+            Yarar-Fisher&apos;s lab in Fall 2019. My primary research interest is
             investigating the effects of neuromuscular electrical stimulation on
             muscle activity and metabolic health after SCI. My future goal is to
             be an educator and researcher in Neuromuscular Physical Therapy with
@@ -215,7 +215,7 @@ const People = () => {
           </CenterDiv>
           <P>
             I am a 4th year post-doctoral scholar at Rehabilitation Medicine
-            Laboratory. I am from China, and I earned my master’s degree in
+            Laboratory. I am from China, and I earned my master&apos;s degree in
             Molecular Genetics from the University of Cincinnati and my PhD
             degree in Nutrition Sciences from Purdue University. During my PhD
             training, I was involved with clinical trials investigating several
@@ -223,7 +223,7 @@ const People = () => {
             greatly appreciated the applicability of human clinical nutrition
             research. In the meanwhile, I became interested in exploring the
             underlying mechanisms for the development of cardio-metabolic
-            diseases. I joined Dr. Yarar-Fisher’s lab in 2017 to further expand
+            diseases. I joined Dr. Yarar-Fisher&apos;s lab in 2017 to further expand
             my training in nutrition research and gain additional skillset in
             microbiome and metabolomics analyses. My current research projects
             include: 1) investigating the effects of a
@@ -257,7 +257,7 @@ const People = () => {
           <P>
             I am a second-year Doctorate of Physical Therapy student at the
             University of Alabama at Birmingham (UAB) in Birmingham, AL. I
-            received my bachelor’s degree in Psychology with minors in
+            received my bachelor&apos;s degree in Psychology with minors in
             Neuroscience and Chemistry from the University of Mississippi (Hotty
             Toddy!). I am from Gulfport, MS, and I love the water. During my
             undergraduate career, I conducted research in a behavioral
@@ -300,13 +300,13 @@ const People = () => {
             concert bands, captain of cheerleading team, beta club, FBLA, and
             Student Government Association. In college, I am a cheerleader, an
             ambassador, and I am involved in the STEM Program which has allotted
-            the opportunity of receiving an internship through UAB’s Blazing to
+            the opportunity of receiving an internship through UAB&apos;s Blazing to
             Biomedical Careers (BBC) program as part of a NIH initiative to
             lessen disparities. This program provides an entrance into advanced
             biomedical research careers. The internship led me to join Dr.
-            Yarar’s lab for the summer. I felt her lab would provide guidance
+            Yarar&apos;s lab for the summer. I felt her lab would provide guidance
             into a biomedical career as I hope to become a laboratory personnel
-            or occupational therapist one day. Joining Dr. Yarar-Fisher’s lab
+            or occupational therapist one day. Joining Dr. Yarar-Fisher&apos;s lab
             has allowed me to explore both laboratory and clinical research to
             help guide my future options. My hobbies include cheering, shopping,
             eating, reading, and going to church.
@@ -337,13 +337,13 @@ const People = () => {
             from Mountain Brook High School in 2019. My undergraduate courses in
             Health and Exercise Science have led me to become more interested in
             the role of nutrition and exercise in health and disease, which is
-            what prompted me to join Dr. Yarar-Fisher’s lab. I love being a part
+            what prompted me to join Dr. Yarar-Fisher&apos;s lab. I love being a part
             of this lab because I am able to learn more about basic science
             research techniques, while I am also able to experience the clinical
             side of research and get a glimpse into the tangible effects this
             research can have on individuals. Outside of school, I love playing
             tennis, running, cooking and baking, traveling, and exploring
-            Birmingham’s amazing restaurants with my family and friends!
+            Birmingham&apos;s amazing restaurants with my family and friends!
           </P>
         </CardContainer>
       </CenterDiv>
@@ -368,14 +368,14 @@ const People = () => {
             I received my first B.S. degree in chemistry education from
             Hacettepe University in Turkey. Following my undergraduate education
             I worked as a Histo-technologist at Boston University Medical
-            Center’s Anatomic Pathology Lab for 12 years. Specifically, I worked
+            Center&apos;s Anatomic Pathology Lab for 12 years. Specifically, I worked
             in immunohistochemistry, histology, cytology, and molecular
             diagnostic laboratories. While working as a Histo-technologist I
             earned my second B.S. degree in Biomedical Sciences and Clinical
             Laboratories from Boston University. I moved to Birmingham in August
-            2014 to get my Master’s Degree in Biotechnology at UAB, which I
-            received in August 2015. Following my Master’s Degree I joined Dr.
-            Yarar-Fisher’s laboratory as a laboratory manager. My current
+            2014 to get my Master&apos;s Degree in Biotechnology at UAB, which I
+            received in August 2015. Following my Master&apos;s Degree I joined Dr.
+            Yarar-Fisher&apos;s laboratory as a laboratory manager. My current
             projects investigate the effects of long-term neuromuscular
             electrical stimulation and high protein diet on metabolic and
             hypertrophic signaling in the paralyzed muscle.
@@ -403,13 +403,13 @@ const People = () => {
             I received my B.S degree in Physical Therapy and Rehabilitation from
             Hacettepe University in Turkey. Following graduation, I started
             working as a research assistant at Hacettepe University in 2013. I
-            completed my Master’s degree in June, 2016. The focus of my thesis
+            completed my Master&apos;s degree in June, 2016. The focus of my thesis
             was investigating the effect of exercise after radiofrequency
             denervation in patients with facet joint syndromme. I am still
             working at the same university as a research assistant while
             pursuing a PhD in Rehabilitation Science. My research focus is on
             spinal cord and traumatic brain injury rehabilitation. I joined Dr.
-            Ceren Yarar-Fisher’s lab in September, 2017, and I’m currently
+            Ceren Yarar-Fisher&apos;s lab in September, 2017, and I&apos;m currently
             studying the effects of neuromuscular electrical stimulation on
             muscle metabolism in individuals with sub-acute spinal cord injury.
           </P>
@@ -442,7 +442,7 @@ const People = () => {
             patients with Multiple Sclerosis. I have continued my studies at
             Hacettepe University and started my doctoral training in the field
             of neurological rehabilitation. I am currently studying at Dr. Ceren
-            Yarar-Fisher’s Lab and investigating the effect of neuromuscular
+            Yarar-Fisher&apos;s Lab and investigating the effect of neuromuscular
             electrical stimualtion on muscle metabolism in individuals with
             sub-acute spinal cord injury.
           </P>
@@ -496,10 +496,10 @@ const People = () => {
             spinal cord injuries and the examination of novel nutritional
             modalities and their effects on protection and recovery from damage,
             as well as brown adipose tissue distribution, functionality, and
-            activity. Personally observing how dramatically a patient’s mobility
+            activity. Personally observing how dramatically a patient&apos;s mobility
             can be altered by a single event and then being able to explore
             questions and seek answers that may lead to improvements in such
-            patients’ quality of life is a reward without parallel.
+            patients&apos; quality of life is a reward without parallel.
           </P>
         </CardContainer>
       </CenterDiv>
@@ -534,7 +534,7 @@ const People = () => {
             pursuing as a career. Taking neuroscience courses as an
             undergraduate gave me immense enthusiasm for medicine involving the
             nervous system and played a significant role in my decision to join
-            Dr. Yarar-Fisher’s lab. Through the Department of Physical Medicine
+            Dr. Yarar-Fisher&apos;s lab. Through the Department of Physical Medicine
             and Rehabilitation Functional Neurorehabilitation Research Program,
             I received the FNR Scholarship award to do research this summer. My
             research will be investigating the effectiveness of a ketogenic diet
@@ -568,11 +568,11 @@ const People = () => {
           </CenterDiv>
           <P>
             I am a graduate student in the Department of Nutrition Science
-            rotating in Dr. Yarar-Fisher’s lab. I grew up in Miami, Florida, and
+            rotating in Dr. Yarar-Fisher&apos;s lab. I grew up in Miami, Florida, and
             attended Appalachian State University in Boone, NC earning my B.S.
             in Nutrition and Foods, and Exercise Science as well as my M.S. in
             Dietetics. I am incredibly excited and appreciative for the
-            opportunity to work in Dr. Yarar-Fisher’s lab. Rehabilitation
+            opportunity to work in Dr. Yarar-Fisher&apos;s lab. Rehabilitation
             Medicine is an area of research that has always interested me, but I
             have previously not had the opportunity to be exposed to. As a
             lifelong competitive athlete, I am interested in examining the
